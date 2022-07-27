094655292660b2cf834dd475683d92b864eae339
Mtalkhnani <mtalkhnani@hotmail.com>
2022-07-27 05:22:43 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int lDNum = 1;
+  int rDNum = 1;
+  void changeNum() {
+    lDNum = Random().nextInt(6) + 1;
+    rDNum = Random().nextInt(6) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    changeNum();
+                  });
+                },
+                child: Image(
+                  image: AssetImage('images/dice$lDNum.png'),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    changeNum();
+                  });
+                },
+                child: Image(
+                  image: AssetImage('images/dice$rDNum.png'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
